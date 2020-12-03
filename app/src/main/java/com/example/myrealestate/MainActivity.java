1ee4a4e265edcad77c0095d8082884a5d8b48d97
Zenibaa57 <60871005+Zenibaa57@users.noreply.github.com>
2020-12-03 10:40:52 +0100

    Add Sign In layout

--- app/src/main/java/com/example/myrealestate/MainActivity.java
+++ app/src/main/java/com/example/myrealestate/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.myrealestate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import com.example.myrealestate.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        Window window = this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.red));
         setContentView(R.layout.activity_main);
+
     }
 }
