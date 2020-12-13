fc47bf7e93ffe393ca815040372791ac3129d73f
Zenibaa57 <60871005+Zenibaa57@users.noreply.github.com>
2020-12-13 17:27:24 +0100

    Fix add property view

--- app/src/main/java/com/example/myrealestate/PropertyFormActivity.java
+++ app/src/main/java/com/example/myrealestate/PropertyFormActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 
 
 public class PropertyFormActivity extends AppCompatActivity {
 
+    private CardView cardGarage;
+    private CardView cardHouse;
+    private CardView cardBuilding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,15 @@ public class PropertyFormActivity extends AppCompatActivity {
         Window window = this.getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.red));
         setContentView(R.layout.property_details);
+
+        cardGarage = findViewById(R.id.cardGarage);
+        cardHouse = findViewById(R.id.cardHouse);
+        cardBuilding = findViewById(R.id.cardBuilding);
+
+        if (cardGarage.isSelected()){
+
+        }
+
 
     }
 }
